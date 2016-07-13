92761eb531e7eb9664c540179d6c9dca748d06fe
BowiEgo <bobosndr@163.com>
2016-07-13 16:18:28 +0800

    Site updated: 2016-07-13 16:18:27

---
+++ works/quote/js/share.js
@@ -0,0 +1,44 @@
+window._bd_share_config = {
+		common : {
+			//此处放置通用设置
+			bdText : '',	
+			bdDesc : '',	
+			bdUrl : '', 	
+			bdPic : '',		
+		},
+		share : [
+			//此处放置分享按钮设置
+			{
+			"tag" : "share_1",
+			"bdSize" : 32,
+			"bdCustomStyle" : "css/style.css",
+			},
+		],
+		// slide : [
+		// 	//此处放置浮窗分享设置
+		// 	{	   
+		// 	bdImg : 0,
+		// 	bdPos : "right",
+		// 	bdTop : 100
+		// 	},
+		// 	{
+		// 	bdImg : 0,
+		// 	bdPos : "left",
+		// 	bdTop : 100
+		// 	}
+		// ],
+		image : [
+			//此处放置图片分享设置
+		],
+		selectShare : [
+			//此处放置划词分享设置
+		]
+}
+
+with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?cdnversion='+~(-new Date()/36e5)];
+
+
+
+
+
+
