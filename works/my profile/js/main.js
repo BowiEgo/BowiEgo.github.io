1395d5ed9eb10e1c1c9414204584d1ae12622205
BowiEgo <bobosndr@163.com>
2016-08-11 10:16:00 +0800

    Site updated: 2016-08-11 10:15:59

--- works/my profile/js/main.js
+++ works/my profile/js/main.js
@@ -86,8 +86,7 @@ window.onload = function() {
 				//轮播图
 				var btnL = document.querySelector(".btn-l"),
 					btnR = document.querySelector(".btn-r"),
-					imgList = document.querySelector(".img-container").getElementsByTagName("ul")[0],
-					imgClone = clone(imgList.getElementsByTagName("li"));
+					imgList = document.querySelector(".img-container").getElementsByTagName("ul")[0];
 				var oDot = document.querySelector(".dot").getElementsByTagName("li");
 				var imgWid = imgList.getElementsByTagName("li")[0].offsetWidth;
 				console.log(imgWid);
